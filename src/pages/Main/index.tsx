be3fc33fbe94074017ae630bde18ed3e9aaeafa6
Milton Yano <miltonyano@gmail.com>
2020-10-02 11:17:15 -0400

    Logic to check stored vars changed

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,12 +3,12 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { useHistory } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { useToast } from '../../hooks/toast';
 
+import Header from '../../components/Header';
 import Textarea from '../../components/Textarea';
 import Button from '../../components/Button';
 
@@ -77,11 +77,14 @@ const Main: React.FC = () => {
 
         const { recipe } = data;
 
-        if (recipe !== storedRecipe) {
+        const storedUnitGroup = localStorage.getItem(
+          '@ConvertMyRecipe:unitGroup',
+        );
+
+        if (recipe !== storedRecipe || !storedUnitGroup) {
           localStorage.setItem('@ConvertMyRecipe:recipe', recipe);
           localStorage.removeItem('@ConvertMyRecipe:parsedRecipe');
           localStorage.removeItem('@ConvertMyRecipe:unitGroup');
-          localStorage.removeItem('@ConvertMyRecipe:unitSpanIds');
 
           const response = await api.post<ApiResponse>('/recipe/parse', {
             recipe,
@@ -89,27 +92,21 @@ const Main: React.FC = () => {
 
           const { unitGroup, parsedRecipe } = response.data;
 
-          if (unitGroup.length) {
-            localStorage.setItem('@ConvertMyRecipe:parsedRecipe', parsedRecipe);
-            localStorage.setItem(
-              '@ConvertMyRecipe:unitGroup',
-              JSON.stringify(unitGroup),
-            );
+          if (!unitGroup.length) {
+            addToast({
+              type: 'info',
+              title: 'No units found',
+              description: 'No units where found in your recipe!',
+            });
+
+            return;
           }
-        }
 
-        const storedUnitGroup = localStorage.getItem(
-          '@ConvertMyRecipe:unitGroup',
-        );
-
-        if (!storedUnitGroup || !storedUnitGroup.length) {
-          addToast({
-            type: 'info',
-            title: 'No units found',
-            description: 'No units where found in your recipe!',
-          });
-
-          return;
+          localStorage.setItem('@ConvertMyRecipe:parsedRecipe', parsedRecipe);
+          localStorage.setItem(
+            '@ConvertMyRecipe:unitGroup',
+            JSON.stringify(unitGroup),
+          );
         }
 
         history.push('/recipe');
@@ -134,20 +131,7 @@ const Main: React.FC = () => {
 
   return (
     <Container>
-      <Navbar expand="lg" bg="light">
-        <Navbar.Brand>Convert my Recipe</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <NavDropdown title="Units" id="collasible-nav-dropdown">
-              <NavDropdown.Item>Kilogrm</NavDropdown.Item>
-              <NavDropdown.Item>Gram</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item>Litre</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Header />
 
       <Content>
         <SideAds>
