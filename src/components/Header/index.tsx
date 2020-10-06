2926abcca7d792deab82d62776e6662ff9b9bdea
Milton Yano <miltonyano@gmail.com>
2020-10-06 15:13:23 -0400

    Header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,11 +9,30 @@ const Header: React.FC = () => (
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav>
         <Nav.Link href="/">Home</Nav.Link>
-        <NavDropdown title="Units" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="/#test">Kilogrm</NavDropdown.Item>
-          <NavDropdown.Item>Gram</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item>Litre</NavDropdown.Item>
+        <NavDropdown title="Mass" id="mass-nav-dropdown">
+          <NavDropdown.Item href="/#milligram">Milligram</NavDropdown.Item>
+          <NavDropdown.Item href="/#gram">Gram</NavDropdown.Item>
+          <NavDropdown.Item href="/#kilogram">Kilogram</NavDropdown.Item>
+          <NavDropdown.Item href="/#poundmass">Poundmass</NavDropdown.Item>
+          <NavDropdown.Item href="/#ounce">Ounce</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Volume" id="volume-nav-dropdown">
+          <NavDropdown.Item href="/#milliliter">Milliliter</NavDropdown.Item>
+          <NavDropdown.Item href="/#liter">Liter</NavDropdown.Item>
+          <NavDropdown.Item href="/#teaspoon">Teaspoon</NavDropdown.Item>
+          <NavDropdown.Item href="/#tablespoon">Tablespoon</NavDropdown.Item>
+          <NavDropdown.Item href="/#fluidounce">Fluidounce</NavDropdown.Item>
+          <NavDropdown.Item href="/#cup">Cup</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Length" id="length-nav-dropdown">
+          <NavDropdown.Item href="/#centimeter">Centimeter</NavDropdown.Item>
+          <NavDropdown.Item href="/#meter">Meter</NavDropdown.Item>
+          <NavDropdown.Item href="/#inch">Inch</NavDropdown.Item>
+          <NavDropdown.Item href="/#Foot">Foot</NavDropdown.Item>
+        </NavDropdown>
+        <NavDropdown title="Temperature" id="temperature-nav-dropdown">
+          <NavDropdown.Item href="/#celsius">Celsius</NavDropdown.Item>
+          <NavDropdown.Item href="/#fahrenheit">Fahrenheit</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
