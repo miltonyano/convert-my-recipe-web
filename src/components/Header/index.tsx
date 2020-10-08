4f63bd730142df5af2d527167da506cd75ac334f
Milton Yano <miltonyano@gmail.com>
2020-10-08 14:42:30 -0400

    Header update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,24 +15,40 @@ const Header: React.FC = () => (
           <NavDropdown.Item href="/#milligram">Milligram</NavDropdown.Item>
           <NavDropdown.Item href="/#poundmass">Poundmass</NavDropdown.Item>
           <NavDropdown.Item href="/#ounce">Ounce</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/#mass-conversion-table">
+            Conversion Table
+          </NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Volume" id="volume-nav-dropdown">
-          <NavDropdown.Item href="/#milliliter">Milliliter</NavDropdown.Item>
           <NavDropdown.Item href="/#liter">Liter</NavDropdown.Item>
+          <NavDropdown.Item href="/#milliliter">Milliliter</NavDropdown.Item>
+          <NavDropdown.Item href="/#fluidounce">Fluidounce</NavDropdown.Item>
           <NavDropdown.Item href="/#teaspoon">Teaspoon</NavDropdown.Item>
           <NavDropdown.Item href="/#tablespoon">Tablespoon</NavDropdown.Item>
-          <NavDropdown.Item href="/#fluidounce">Fluidounce</NavDropdown.Item>
           <NavDropdown.Item href="/#cup">Cup</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/#volume-conversion-table">
+            Conversion Table
+          </NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Length" id="length-nav-dropdown">
-          <NavDropdown.Item href="/#centimeter">Centimeter</NavDropdown.Item>
           <NavDropdown.Item href="/#meter">Meter</NavDropdown.Item>
+          <NavDropdown.Item href="/#centimeter">Centimeter</NavDropdown.Item>
           <NavDropdown.Item href="/#inch">Inch</NavDropdown.Item>
           <NavDropdown.Item href="/#Foot">Foot</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/#length-conversion-table">
+            Conversion Table
+          </NavDropdown.Item>
         </NavDropdown>
         <NavDropdown title="Temperature" id="temperature-nav-dropdown">
           <NavDropdown.Item href="/#celsius">Celsius</NavDropdown.Item>
           <NavDropdown.Item href="/#fahrenheit">Fahrenheit</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="/#temperature-conversion-table">
+            Conversion Table
+          </NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
